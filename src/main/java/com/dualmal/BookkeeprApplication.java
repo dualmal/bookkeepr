370d797bdc0762f3ce485536809739b5caaabbdb
rjc <dualmal@gmail.com>
2016-11-13 00:02:51 -0600

    Initial commit

---
+++ src/main/java/com/dualmal/BookkeeprApplication.java
@@ -0,0 +1,12 @@
+package com.dualmal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkeeprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookkeeprApplication.class, args);
+	}
+}
